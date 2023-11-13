5f55383fdb56641e749d7ea9b17a57cc4e704fdd
Alexandre Gascon <alexandregascon@sfr.fr>
2023-11-13 11:53:55 +0100

    Mise en place du front

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Services\GenerateurNumeroAdherent;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\SchemaTool;
+use Symfony\Component\Validator\Validation;
+
+require_once "vendor/autoload.php";
+require "bootstrap.php";
+
+
+if($_POST["prenom"]){
+
+    // Création du générateur
+    $generateur = new GenerateurNumeroAdherent();
+    // Création du valdateur
+    $validateur = Validation::createValidatorBuilder()
+        ->enableAnnotationMapping()
+        ->addDefaultDoctrineAnnotationReader()
+        ->getValidator();
+
+    $requete = new \App\UserStories\CreerAdherent\CreerAdherentRequete($_POST["prenom"],$_POST["nom"],$_POST["mail"]);
+    $creerAdherent = new \App\UserStories\CreerAdherent\CreerAdherent($entityManager,$generateur,$validateur);
+    $creerAdherent->execute($requete);
+}
+
+?>
+
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Création Adherent</title>
+</head>
+<body>
+    <h1>Créer un adherent</h1>
+    <form action="" method="post">
+        <label for="prenom">Entrer le prénom : </label>
+        <input type="text" name="prenom" id="prenom">
+
+        <label for="nom">Entrer le nom : </label>
+        <input type="text" name="nom" id="nom">
+
+        <label for="mail">Entrer le mail : </label>
+        <input type="text" name="mail" id="mail">
+
+        <input type="submit" value="Créer">
+    </form>
+</body>
+</html>
